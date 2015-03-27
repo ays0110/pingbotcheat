130e4f729b2a1b6e63cf1c83ab76be55fd258f1b
alice <alice@tintup.com>
2015-03-26 18:47:01 -0700

    le

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def ranking
 	@players  = Player.where('status != -1 AND rank > 0').order('rank')
-	render json: { success: true, players: players }, status: 201
+	render json: { success: true, players: @players }, status: 201
   end
 
 end
